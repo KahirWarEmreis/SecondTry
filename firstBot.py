a8103d8967417bcb9131ac750a5304de61fe52c7
KahirWarEmreis <50171832+KahirWarEmreis@users.noreply.github.com>
2019-05-03 23:09:26 +0300

    Update firstBot.py

--- firstBot.py
+++ firstBot.py
@@ -2,6 +2,7 @@ import telebot
 bot = telebot.TeleBot('891721503:AAE784YcJaxoQih6XLVSruOQjdGIFz01AeI')
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
+    print(0)
     if message.text == "Привет":
         bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
     elif message.text == "/help":
